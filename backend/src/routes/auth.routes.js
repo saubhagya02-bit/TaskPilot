64733ed4beb62ee4d28d2392171e7e366f13f0ff
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-07-07 17:18:38 +0530

    Add database connection configuration

--- backend/src/routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const ctrl = require('../controllers/auth.controller');
-
-router.post('/register', ctrl.register);
-router.post('/login', ctrl.login);
-
-module.exports = router;
