3b91c02180103b08ca39fea39a212ad84a572e71
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-07-08 15:20:26 +0530

    Create task service for API integration

--- frontend/src/app/features/tasks/task.service.ts
+++ frontend/src/app/features/tasks/task.service.ts
@@ -1,31 +1,32 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.prod';
 
+export interface Task {
+  id: number;
+  title: string;
+  completed: boolean;
+  date: string | null;
+}
 @Injectable({ providedIn: 'root' })
 export class TaskService {
   private api = 'http://localhost:5000/api/tasks';
 
   constructor(private http: HttpClient) {}
 
-  private getAuthHeaders(): HttpHeaders {
-    const token = localStorage.getItem('token');
-    if (!token) throw new Error('No auth token found. Please log in.');
-    return new HttpHeaders({ Authorization: `Bearer ${token}` });
-  }
-
   getTasks() {
-    return this.http.get<any[]>(this.api, { headers: this.getAuthHeaders() });
+    return this.http.get<Task[]>(this.api);
   }
 
-  addTask(task: any) {
-    return this.http.post<any>(this.api, task, { headers: this.getAuthHeaders() });
+  addTask(task: Partial<Task>) {
+    return this.http.post<any>(this.api, task);
   }
 
   updateTask(id: number, task: any) {
-    return this.http.put<any>(`${this.api}/${id}`, task, { headers: this.getAuthHeaders() });
+    return this.http.put<any>(`${this.api}/${id}`, task);
   }
 
   deleteTask(id: number) {
-    return this.http.delete<any>(`${this.api}/${id}`, { headers: this.getAuthHeaders() });
+    return this.http.delete<any>(`${this.api}/${id}`);
   }
 }
