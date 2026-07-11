b38eec4fe58b460fc9dc2f59a9b08c1c22f10ad1
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-07-11 11:23:05 +0530

    Configure Angular application providers

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,19 +1,16 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
-import { provideHttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
+import { authInterceptor } from './core/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimations(),
-    importProvidersFrom(FormsModule),
-    
-     provideToastr()
-  ]
+    provideToastr(),
+  ],
 };
