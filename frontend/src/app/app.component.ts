ba9cb7c609c91de3b392c9bdc54aa8efc517ebed
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-07-11 11:22:26 +0530

    Configure root application component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,22 +1,11 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
-import { AuthService } from './core/auth.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-
-    constructor(private auth: AuthService, private router: Router) {}
-
-    ngOnInit() {
-        if (this.auth.isLoggedIn()) {
-            this.router.navigate(['/tasks']);
-        } else {
-            this.router.navigate(['/login']);
-        }
-    }
-}
+export class AppComponent {}
